3b1b325da9ae458e01df164fc1ed7fcdde108a74
Reed W Priest <pries153@umn.edu>
2023-04-28 14:40:05 -0500

    3rd and 4th questions done for SQL.

--- R/week13-SQL.R
+++ R/week13-SQL.R
@@ -22,9 +22,15 @@ dbGetQuery(con, "SELECT COUNT(DISTINCT employee_id)
            FROM cla_tntlab.datascience_8960_table;")
 
 # Display a summary of the number of managers split by location, but only include those who were not originally hired as managers.
-
+dbGetQuery(con, "SELECT COUNT(employee_id), city
+           FROM cla_tntlab.datascience_8960_table
+           WHERE manager_hire = 'N'
+           GROUP BY city;")
 
 # Display the average and standard deviation of number of years of employment split by performance level (bottom, middle, and top).
-
+# The numbers slightly differed between procedures, which may be due to rounding differences.
+dbGetQuery(con, "SELECT performance_group, AVG(yrs_employed), STDDEV(yrs_employed)
+           FROM cla_tntlab.datascience_8960_table
+           GROUP BY performance_group;")
 
 # Display the location and ID numbers of the top 3 managers from each location, in alphabetical order by location and then descending order of test score. If there are ties, include everyone reaching rank 3.
